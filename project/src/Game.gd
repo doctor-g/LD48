3828820d57204398820d445a7f9d1ff5c2789901
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-24 09:16:48 -0400

    Adjust world size

--- project/src/Game.gd
+++ project/src/Game.gd
@@ -1,7 +1,7 @@
 extends Node
 
-const WIDTH  := 10
-const HEIGHT := 10
+const WIDTH  := 15
+const HEIGHT := 14
 const TILE_WIDTH := 64
 const TILE_HEIGHT := 64
 const TILE_SIZE := Vector2(TILE_WIDTH, TILE_HEIGHT)
