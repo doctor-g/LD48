ce2a9d34f6c4fce10bc5dfcf2ad644434779d125
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-24 11:27:20 -0400

    Unstun after a delay

--- project/src/Enemy.gd
+++ project/src/Enemy.gd
@@ -7,6 +7,8 @@ var _stunned := false
 
 var _color := Color.green
 
+onready var _stun_timer := $StunTimer
+
 func _draw():
 	draw_rect(Rect2(0,0,Game.TILE_WIDTH, Game.TILE_HEIGHT), _color)
 
@@ -29,8 +31,21 @@ func stun():
 	_stunned = true
 	_color = Color.bisque
 	update() # Force draw to be called
+	
+	# If it was previously stunned, add stun length to it
+	if _stun_timer.time_left >= 0:
+		_stun_timer.start()
+	else:
+		_stun_timer.time_left = _stun_timer.wait_time
+		
 
 
 func damage():
 	print("Enemy has died")
 	queue_free()
+
+
+func _on_StunTimer_timeout():
+	_stunned = false
+	_color = Color.green
+	update()
