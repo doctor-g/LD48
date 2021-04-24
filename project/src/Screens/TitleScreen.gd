56ffd2ab663ebfba80f123fb746e9d8c05f928fa
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-24 15:32:15 -0400

    Style title screen
    This required changing from OpenSans to NotoSansJP so that the
    input help text would have arrow symbols

--- project/src/Screens/TitleScreen.gd
+++ project/src/Screens/TitleScreen.gd
@@ -1,6 +1,11 @@
 extends Control
 
+var clicks_remaining := 2
 
-func _on_Button_pressed():
-	Game.start()
-	get_tree().change_scene("res://src/GameScreen.tscn")
+
+func _on_ClickinLabel_done():
+	clicks_remaining -= 1
+	print("clicks remaining " + str(clicks_remaining))
+	if clicks_remaining <= 0:
+		Game.start()
+		get_tree().change_scene("res://src/GameScreen.tscn")
