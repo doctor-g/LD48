74acce84ea78b142d97a2a0fc8604124051e9c40
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-24 09:54:51 -0400

    Add barrier to prevent enemy moving off board

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -4,6 +4,9 @@ var player_spawn_points := [ 0,0, 6,0]
 var open_points := [1,4, 2,4, 3,4]
 
 func _ready():
+	_add_barriers()
+	
+	# Populate the level
 	for i in range(0, Game.WIDTH):
 		for j in range(0, Game.HEIGHT):
 			if not _is_player_spawn_point(i,j) and not _is_open_point(i,j):
@@ -27,6 +30,28 @@ func _ready():
 	enemy.position.y = open_points[1] * Game.TILE_HEIGHT
 	add_child(enemy)
 
+
+func _add_barriers():
+	for i in range(0, Game.WIDTH):
+		_add_barrier(i, -1)
+		_add_barrier(i, Game.HEIGHT+1)
+	for j in range(0, Game.HEIGHT):
+		_add_barrier(-1, j)
+		_add_barrier(Game.WIDTH+1, j)
+
+
+func _add_barrier(x:int, y:int)->void:
+	var barrier := StaticBody2D.new()
+	barrier.add_to_group("tiles")
+	var collision := CollisionShape2D.new()
+	collision.position = Game.TILE_SIZE / 2
+	collision.shape = RectangleShape2D.new()
+	collision.shape.extents = Game.TILE_SIZE / 2
+	barrier.add_child(collision)
+	barrier.position.x = Game.TILE_WIDTH * x
+	barrier.position.y = Game.TILE_HEIGHT * y
+	add_child(barrier)
+	
 
 func _is_player_spawn_point(x:int, y:int)->bool:
 	return _is_in_serial_point_list(x,y,player_spawn_points)
