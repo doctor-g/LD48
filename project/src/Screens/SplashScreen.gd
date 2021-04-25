e178b493352358a5708791cb2552f3ddb1fb4a52
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-25 15:21:59 -0400

    Show splash screen on HTML5 build only

---
+++ project/src/Screens/SplashScreen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+const TITLE = preload("res://src/Screens/TitleScreen.tscn")
+
+# We only need to do the click-through on HTML5.
+func _ready():
+	if OS.get_name() != "HTML5":
+		_go_to_title_screen()
+		
+
+func _input(event):
+	if (event is InputEventMouseButton and event.is_pressed()) \
+		or event.is_action_released("p1_fire") \
+		or event.is_action_released("p2_fire"):
+			$AnimationPlayer.play("Fall")
+
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	_go_to_title_screen()
+	
+
+func _go_to_title_screen():
+	get_tree().change_scene_to(TITLE)
